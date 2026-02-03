3b95a41c5ebf46e3822042c923bbfecbb8620569
ahn <sharkwind1@gmail.com>
2026-02-03 14:15:42 +0900

    모달 로딩 상태 초기화 버그 수정

--- src/components/Auth/AuthModal.tsx
+++ src/components/Auth/AuthModal.tsx
@@ -57,6 +57,7 @@ export default function AuthModal({
             setNickname("");
             setError(null);
             setSuccess(null);
+            setLoading(false);
         }
     }, [isOpen]);
 
